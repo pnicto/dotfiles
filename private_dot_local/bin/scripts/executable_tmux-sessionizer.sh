0e885dc8b4998305d858b851286279aa4fa2e612
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-08-04 12:53:55 +0530

    Update .local/bin/scripts/tmux-sessionizer.sh

--- private_dot_local/bin/scripts/executable_tmux-sessionizer.sh
+++ private_dot_local/bin/scripts/executable_tmux-sessionizer.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 session_dir=$(find ~ ~/programs/ ~/work/ ~/learning/ ~/playground/ ~/.local/bin/ ~/.config/ "/run/media/hans/OS/Users/ydkr7/College/Sem 4/" -mindepth 1 -maxdepth 1 -type d | fzf)
+exit_status=$?
 session_name=$(basename "$session_dir" | tr . _)
 
-if ! tmux has-session -t "$session_name" 2>/dev/null; then
-    tmux new-session -s "$session_name" -c "$session_dir" -d
+if [ $exit_status -eq 0 ]; then
+    if ! tmux has-session -t "$session_name" 2>/dev/null; then
+        tmux new-session -s "$session_name" -c "$session_dir" -d
+    fi
+    tmux switch-client -t "$session_name"
+else
+    exec $SHELL
 fi
-
-tmux switch-client -t "$session_name"
