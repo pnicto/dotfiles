6346e56573111016943b7776b24e15b3ee5d6238
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-08-27 11:45:43 +0530

    Update .local/bin/scripts/cms.sh

--- private_dot_local/bin/scripts/executable_cms.sh
+++ private_dot_local/bin/scripts/executable_cms.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# shellcheck source=./.env
 source ~/.local/bin/scripts/.env
 
 url="https://cms.bits-hyderabad.ac.in/webservice/rest/server.php?wsfunction=message_popup_get_popup_notifications&moodlewsrestformat=json&wstoken=${CMS_TOKEN}&useridto=9560"
@@ -9,9 +10,11 @@ notifications_count=$(curl --silent -X POST "$url" | jq -r '.unreadcount')
 # to get notifications I don't remember but I had to do this
 XDG_RUNTIME_DIR=/run/user/$(id -u)
 
+export XDG_RUNTIME_DIR
+
 if ((notifications_count > 0)); then
-	dunstify "CMS" "You have $notifications_count notifications" -u critical
-	echo "CMS $notifications_count"
+    dunstify "CMS" "You have $notifications_count notifications" -u critical
+    echo "CMS $notifications_count"
 else
-	echo ""
+    echo "CMS $notifications_count"
 fi
