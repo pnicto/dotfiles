70c9cd9cf0d9496c18fd363f38eff80fdcbfac05
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2023-12-09 08:57:31 +0530

    Update .local/bin/scripts/startup-apps.sh

--- private_dot_local/bin/scripts/executable_startup-apps.sh
+++ private_dot_local/bin/scripts/executable_startup-apps.sh
@@ -3,20 +3,6 @@
 # shellcheck source=./.env
 source ~/.local/bin/scripts/.env
 
-obsidian &
-
-qbittorrent &
-
-komga --komga.config-dir="$HOME/.config/komga" --server.port=8443 &
-
-airdcppd &
-
-sleep 5 && aw-at &
-
-discord --start-minimized &
-
-imwheel -d
-
 curl 'http://172.16.0.30:8090/login.xml' \
 	-H 'Accept: */*' \
 	-H 'Accept-Language: en-GB,en;q=0.9,en-US;q=0.8' \
@@ -28,4 +14,20 @@ curl 'http://172.16.0.30:8090/login.xml' \
 	-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0' \
 	--data-raw "mode=191&username=$SOPHOS_USER&password=$SOPHOS_PASSWORD&a=1701656918323&producttype=0" \
 	--compressed \
-	--insecure
+	--insecure &
+
+obsidian &
+
+qbittorrent &
+
+komga --komga.config-dir="$HOME/.config/komga" --server.port=8443 &
+
+thunderbird --headless &
+
+airdcppd &
+
+sleep 5 && aw-at &
+
+discord --start-minimized &
+
+imwheel -d
