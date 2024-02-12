4d5b5619b445e2f7878a97e2ef4e7974912a28fe
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-12 10:39:28 +0530

    Update .local/bin/scripts/total-watch-time.sh

--- private_dot_local/bin/scripts/executable_total-watch-time.sh
+++ private_dot_local/bin/scripts/executable_total-watch-time.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+directory="$1"
+
 total_seconds=0
-for dir in */; do
-	videos=$(find "$dir" -type f -name "*.mkv" -or -name "*.mp4")
+
+for dir in $directory; do
+	videos=$(find "$dir" -type f -name "*.mkv" -or -name "*.mp4" -or -name "*.webm")
 	total_duration=0
 	IFS=$'\n'
 	for video in $videos; do
 		duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$video" 2>/dev/null)
+		echo "$video $(echo "$duration" / 60 | bc -l)"
 
 		if [ -n "$duration" ]; then
 			total_duration=$(echo "$total_duration + $duration" | bc -l)
