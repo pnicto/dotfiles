7952523b7a5e2513208a9cdbcd30e267bf9b66e0
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-02-25 00:09:57 +0530

    Update .local/bin/scripts/workspaces.sh

--- private_dot_local/bin/scripts/executable_workspaces.sh
+++ private_dot_local/bin/scripts/executable_workspaces.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-sessions=$(jq -r '.sessions | keys[]' sessions.json)
+json_file=~/.local/bin/scripts/sessions.json
+
+sessions=$(jq -r '.sessions | keys[]' "$json_file")
 
 selected_session=$(echo "$sessions" | rofi -dmenu -p "session" -no-custom -config ~/.config/rofi/arc_dark_transparent_colors.rasi)
 
-commands=$(jq -r ".sessions[\"$selected_session\"][]" sessions.json)
+commands=$(jq -r ".sessions[\"$selected_session\"][]" "$json_file")
 
 for action in "$commands"; do
 	eval "$action" &
