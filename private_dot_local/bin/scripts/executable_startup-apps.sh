bdde58b84a2b1674681c53c43437ad926d4a9305
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-07-28 23:17:25 +0530

    Update .local/bin/scripts/startup-apps.sh

--- private_dot_local/bin/scripts/executable_startup-apps.sh
+++ private_dot_local/bin/scripts/executable_startup-apps.sh
@@ -4,17 +4,17 @@
 source ~/.local/bin/scripts/.env
 
 curl 'http://172.16.0.30:8090/login.xml' \
-	-H 'Accept: */*' \
-	-H 'Accept-Language: en-GB,en;q=0.9,en-US;q=0.8' \
-	-H 'Connection: keep-alive' \
-	-H 'Content-Type: application/x-www-form-urlencoded' \
-	-H 'DNT: 1' \
-	-H 'Origin: http://172.16.0.30:8090' \
-	-H 'Referer: http://172.16.0.30:8090/httpclient.html' \
-	-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0' \
-	--data-raw "mode=191&username=$SOPHOS_USER&password=$SOPHOS_PASSWORD&a=1701656918323&producttype=0" \
-	--compressed \
-	--insecure &
+  -H 'Accept: */*' \
+  -H 'Accept-Language: en-GB,en;q=0.9,en-US;q=0.8' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'DNT: 1' \
+  -H 'Origin: http://172.16.0.30:8090' \
+  -H 'Referer: http://172.16.0.30:8090/httpclient.html' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0' \
+  --data-raw "mode=191&username=$SOPHOS_USER&password=$SOPHOS_PASSWORD&a=1701656918323&producttype=0" \
+  --compressed \
+  --insecure &
 
 obsidian &
 
@@ -26,7 +26,7 @@ thunderbird --headless &
 
 # rog-control-center &
 
-airdcppd &
+# airdcppd &
 
 sleep 5 && aw-at &
 
