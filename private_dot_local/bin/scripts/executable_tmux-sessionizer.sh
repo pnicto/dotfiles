d26cef94ccaf59fbcf5d130358ca9c4f8e306eae
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-10-10 11:53:54 +0530

    Update .local/bin/scripts/tmux-sessionizer.sh

--- private_dot_local/bin/scripts/executable_tmux-sessionizer.sh
+++ private_dot_local/bin/scripts/executable_tmux-sessionizer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-session_dir=$(find ~ ~/programs/ ~/work/ ~/learning/ ~/scratch/ ~/.local/bin/ ~/.config/ ~/college/ -mindepth 1 -maxdepth 1 -type d | fzf)
+session_dir=$(find ~ ~/programs/ ~/work/ ~/learning/ ~/scratch/ ~/.local/bin/ ~/.config/ ~/college/ -mindepth 1 -maxdepth 1 -type d | fzf --cycle --layout=reverse)
 exit_status=$?
 session_name=$(basename "$session_dir" | tr . _)
 
